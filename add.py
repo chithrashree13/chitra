b7e3315784a1124048045389c337d605663cf945
chithrashree13 <117499513+chithrashree13@users.noreply.github.com>
2022-12-16 12:17:44 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,10 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+n1=input("enter n1")
+enter n1 90
+n2=input("enter n2")
+enter n2 56
+sum=n1+n2
+print("the sum of two numbers is",format(sum))
+the sum of two numbers is  90 56
+
